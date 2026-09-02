7d28d597a8cc16594d9dfe2afae944907c19f8c7
pranathisreekakumani <pranathisree.kakumani@gmail.com>
2026-09-02 12:45:21 +0530

    Add files via upload

---
+++ assignmentoperator.py
@@ -0,0 +1,24 @@
+x=10
+
+x += 5
+print(x)
+
+x -= 3
+print(x)
+
+x *= 2
+print(x)
+
+x *=3
+print(x)
+#bank balence
+balence =100000
+
+deposit =5000
+balence += deposit
+
+print("balence after deposit:", balence)
+
+withdrw = 2000
+balence -= withdrw
+print("balence after withdrawal:", balence)
---
+++ python.py
@@ -0,0 +1,57 @@
+a=10 
+b=3
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+#simple calculator
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+
+ 
+print("HRA:", hra)
+print("DA:", da)
+print("Gross Salary:", gross_salary)#student marks calculator
+name =input("enter student name:")
+m1 =int(input("enter python marks:"))
+m2 =int(input("enter java marks:"))
+m3 =int(input("enter SQL marks:"))
+total =m1+m2+m3
+average = total/3
+
+print("\n------student report ------")
+print("student name:", name)
+print("total:", total)
+print("average:", average)
+#shopping bill calculator
+price1 = float(input("enter price of item 1:"))
+price2 = float(input("enter price of item 2:"))
+price3 =float(input("enter price of item 3:"))
+total = price1 + price2 + price3
+discount = total * 0.10
+finalamount =total - discount
+#salary calculator
+basic = float(input("enter basic salary:"))
+hra = basic * 0.20
+da = basic * 0.10
+gross_salary = basic + hra + da
+print("basic salary:", basic)
+salarycalculator
+basic = float(input("enter basic salary:"))
+
+hra =basic * 0.20
+da = basic * 0.10
+
+gross_salary = basic + hra + da
+print("basic salary:",basic)
+print("HRA:", hra)
+print("DA:", da)
+print("Gross Salary:", gross_salary)
+#aasignment 
+x=10
